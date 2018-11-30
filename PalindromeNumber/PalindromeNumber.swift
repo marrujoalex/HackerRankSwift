7208e3ff6390f3b0f022ed98c314d0147c4135f7
Alex Marrujo <marrujo.samuel@gmail.com>
2018-11-29 16:53:21 -0800

    This passed LC and was 95% faster than the rest

--- PalindromeNumber/PalindromeNumber.swift
+++ PalindromeNumber/PalindromeNumber.swift
@@ -8,17 +8,19 @@ class Solution {
         // Confirm this value can check for a Palindrome first
         if x < 0 || (x != 0 || x % 10 == 0) { 
             return false 
+        } else if x < 10 {
+            return true
         }
         
         // Passed check, now declare needed variables
         var param = x // x is immutable, needs to mutate
-        var sum = 0
+        var result = 0
 
-        while param > sum {
-            sum = sum * 10 + param % 10
+        while param != 0 {
+            result = result * 10 + param % 10
             param /= 10
         }
 
-        return (param == sum || param == sum / 10)
+        return result == x 
     }
 }
